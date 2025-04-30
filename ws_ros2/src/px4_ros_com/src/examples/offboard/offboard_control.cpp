fa4a04d23d99d0ceb78b7a2e8757fca384246e6b
fidelisprasetyo <fidelisprasetyo@gmail.com>
2025-04-29 21:34:44 -0700

    fingers crossed

--- ws_ros2/src/px4_ros_com/src/examples/offboard/offboard_control.cpp
+++ ws_ros2/src/px4_ros_com/src/examples/offboard/offboard_control.cpp
@@ -22,12 +22,16 @@ using namespace std::chrono;
 using namespace std::chrono_literals;
 using namespace px4_msgs::msg;
 
-// MISSION PARAMETERS
-const int OFFBOARD_LOITER_TIME = 10; // adjust loiter time (seconds)
+// ----------------------------------------------------------------------------------------------------------------------------------------//
+// Adjust as needed
+const seconds OFFBOARD_LOITER_TIME = 10s; // adjust loiter time (seconds)
 const float DISTANCE_THRESHOLD = 10; // not sure if needed tbh (meters)
 const std::vector<std::vector<float>> MISSION_SETPOINTS = {{100.0, 50, -30.0}, {-100, -50, -30}};
+// ----------------------------------------------------------------------------------------------------------------------------------------//
 
-// DON'T TOUCH
+
+
+// ----------------------------------------------------------------------------------------------------------------------------------------//
 class OffboardPlane : public rclcpp::Node {
 public:
   OffboardPlane(const std::vector<std::vector<float>>& local_setpoints) : Node("offboard_plane"), local_setpoints_(local_setpoints) {
@@ -45,8 +49,10 @@ public:
     vehicle_status_subcriber = this->create_subscription<VehicleStatus>("/fmu/out/vehicle_status", qos_profile, std::bind(&OffboardPlane::vehicle_status_callback, this, std::placeholders::_1));
     vehicle_odometry_subscriber = this->create_subscription<VehicleOdometry>("/fmu/out/vehicle_odometry", qos_profile, std::bind(&OffboardPlane::vehicle_odometry_callback, this, std::placeholders::_1));
     
-    std::thread t1(&OffboardPlane::run, this);
-    t1.detach();
+    auto timer_callback = [this]()-> void {
+      run();
+    };
+    timer_ = this->create_wall_timer(100ms, timer_callback);
   }
 
   void run();
@@ -57,6 +63,13 @@ private:
   px4_msgs::msg::VehicleStatus vehicle_status_;
   px4_msgs::msg::VehicleOdometry vehicle_odometry_;
 
+  using clock = std::chrono::steady_clock;
+  clock::time_point loiter_start_time;
+  bool mission_completed = false;
+  bool loiter_mode = false;
+  const std::chrono::seconds loiter_duration = OFFBOARD_LOITER_TIME;
+
+  rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<px4_msgs::msg::OffboardControlMode>::SharedPtr offboard_control_mode_publisher_;
   rclcpp::Publisher<px4_msgs::msg::TrajectorySetpoint>::SharedPtr trajectory_setpoint_publisher_;
   rclcpp::Publisher<px4_msgs::msg::VehicleCommand>::SharedPtr vehicle_command_publisher_;
@@ -69,7 +82,7 @@ private:
   void vehicle_status_callback(const px4_msgs::msg::VehicleStatus::SharedPtr msg);
   void vehicle_odometry_callback(const px4_msgs::msg::VehicleOdometry::SharedPtr msg);
 };
-
+// ----------------------------------------------------------------------------------------------------------------------------------------//
 
 int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
@@ -79,66 +92,62 @@ int main(int argc, char *argv[]) {
   rclcpp::shutdown();
   return 0;
 }
-
+// ----------------------------------------------------------------------------------------------------------------------------------------//
 
 // MEMBER FUNCTIONS DEFINITION
+
+// where the magic happens
 void OffboardPlane::run() {
 
-  using clock = std::chrono::steady_clock;
-  clock::time_point loiter_start_time;
-  bool mission_completed = false;
-  bool loiter_mode = false;
-  const std::chrono::seconds loiter_duration(OFFBOARD_LOITER_TIME);
+  float pos_n = vehicle_odometry_.position[0];
+  float pos_e = vehicle_odometry_.position[1];
+  float pos_d = vehicle_odometry_.position[2];
 
-  while (rclcpp::ok() && !mission_completed) {
-    
-    float pos_n = vehicle_odometry_.position[0];
-    float pos_e = vehicle_odometry_.position[1];
-    float pos_d = vehicle_odometry_.position[2];
+  float x = local_setpoints_[waypoint_index_][0];
+  float y = local_setpoints_[waypoint_index_][1];
+  float z = local_setpoints_[waypoint_index_][2];
 
-    float x = local_setpoints_[waypoint_index_][0];
-    float y = local_setpoints_[waypoint_index_][1];
-    float z = local_setpoints_[waypoint_index_][2];
+  float dx = x - pos_n;
+  float dy = y - pos_e;
+  float dz = z - pos_d;
+  float distance = sqrt(dx * dx + dy * dy + dz * dz);
 
-    float dx = x - pos_n;
-    float dy = y - pos_e;
-    float dz = z - pos_d;
-    float distance = sqrt(dx * dx + dy * dy + dz * dz);
-
-    if(!loiter_mode) {
-      RCLCPP_INFO(this->get_logger(), "Distance to waypoint: %f", distance);
-      if (distance < DISTANCE_THRESHOLD) {
-        loiter_mode = true;
-        loiter_start_time = clock::now();
-      }
-    }
-
-    if(loiter_mode) {
-      auto now = clock::now();
-      auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(now - loiter_start_time);
-      auto remaining = loiter_duration - elapsed;
-
-      RCLCPP_INFO(this->get_logger(), "Loiter mode, time remaining: %ld s", remaining.count());
-
-      if(now - loiter_start_time > loiter_duration) {
-        loiter_mode = false;
-        if(waypoint_index_ + 1 == local_setpoints_.size()) {
-          publish_vehicle_command(VehicleCommand::VEHICLE_CMD_DO_SET_MODE, 1, 4, 3); // SWITCH TO NATIVE HOLD/ LOITER MODE
-          RCLCPP_INFO(this->get_logger(), "Mission completed, switching to Hold mode...");
-          mission_completed = true;
-          return;
-        }
-        waypoint_index_++;
-      }
-    }
-
-    publish_offboard_control_mode();
-    if(vehicle_status_.nav_state == vehicle_status_.NAVIGATION_STATE_OFFBOARD) {
-      publish_trajectory_setpoint(x, y, z);
-    }
-    
-    std::this_thread::sleep_for(std::chrono::milliseconds(100)); // 2Hz
+  if(!mission_completed) {
+    RCLCPP_INFO(this->get_logger(), "Odometry data: N: %f, E: %f, D: %f", pos_n, pos_e, pos_d);
   }
+
+  if(!OffboardPlane::loiter_mode && !mission_completed) {
+    RCLCPP_INFO(this->get_logger(), "Distance to waypoint: %f", distance);
+    if (distance < DISTANCE_THRESHOLD) {
+      loiter_mode = true;
+      loiter_start_time = clock::now();
+    }
+  }
+
+  if(loiter_mode && !mission_completed) {
+    auto now = clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(now - loiter_start_time);
+    auto remaining = loiter_duration - elapsed;
+
+    RCLCPP_INFO(this->get_logger(), "Loiter mode, time remaining: %ld s", remaining.count());
+
+    if(now - loiter_start_time > loiter_duration) {
+      loiter_mode = false;
+      if(waypoint_index_ + 1 == local_setpoints_.size()) {
+        publish_vehicle_command(VehicleCommand::VEHICLE_CMD_DO_SET_MODE, 1, 4, 3); // SWITCH TO NATIVE HOLD/ LOITER MODE
+        RCLCPP_INFO(this->get_logger(), "Mission completed, switching to Hold mode...");
+        mission_completed = true;
+        return;
+      }
+      waypoint_index_++;
+    }
+  }
+
+  publish_offboard_control_mode();
+  if(vehicle_status_.nav_state == vehicle_status_.NAVIGATION_STATE_OFFBOARD) {
+    publish_trajectory_setpoint(x, y, z);
+  }
+  
 }
 
 void OffboardPlane::vehicle_status_callback(const px4_msgs::msg::VehicleStatus::SharedPtr msg) {
